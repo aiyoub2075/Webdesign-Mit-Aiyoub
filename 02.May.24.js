e0cf04417edba80bea408b75c3bbe8c90a8664c9
aiyoub2075 <aiyoub.reichert@edu.techstarter.de>
2024-05-02 18:01:26 +0200

    js

---
+++ 02.May.24.js
@@ -0,0 +1,14 @@
+let x="Aiyoub"
+console.log(x)
+let C = 20
+F = C * 9/5 + 32
+console.log(F)
+let fruits = [];
+fruits.push("Kiwi");
+fruits.push("Apple");
+fruits.push("Banana");
+fruits.push("Birne");
+fruits.push("Grape");
+console.log(fruits)
+for (i of fruits)
+console.log(i)
