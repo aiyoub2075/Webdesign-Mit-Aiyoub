d1277bb6a6ede458db8411bfdb789fded77693f9
aiyoub2075 <aiyoub.reichert@edu.techstarter.de>
2024-05-17 16:19:29 +0200

    Update 17May.js

--- Expressapp/17.May/17May.js
+++ Expressapp/17.May/17May.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 // Statische Dateien bereitstellen
 app.use(express.static(path.join(__dirname, '17.May')));
